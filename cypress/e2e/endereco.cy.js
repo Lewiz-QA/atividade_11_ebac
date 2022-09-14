093b26d04e6dd7b39622a6c5a21c12ab97d8d9a6
Luiz <lgm97.com@gmail.com>
2022-09-13 22:33:56 -0300

    Page Objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,7 +10,8 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         
     });
 
-    it('Deve fazer cadastro do local do faturamento', () => {
-        
+    it.only('Deve fazer cadastro do local do faturamento', () => {
+        EnderecoPage.editarEnderecoFaturamento('Luiz', 'Guilherme', 'EBAC', 'Avenida Brasil', '3010', 'São Paulo', 'São Paulo', '84430000', '4234232020', 'mail@test.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
